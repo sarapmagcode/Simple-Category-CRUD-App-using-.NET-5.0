77851b59af9aa61672f86008d865b0499e185f8f
markg <markg@taxumo.com>
2023-09-20 13:45:39 +0800

    first commit

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,130 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET: Category/Index
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        // GET: Category/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Category/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully!";
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+        }
+
+        // GET: Category/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            // SingleOrDefault() - will throw an exception if there are multiple elements
+            // Single() - will throw an exception if no element is found
+            // FirstOrDefault() - will return the first element (even if there are multiple elements)
+            // Find() - based on the primary key of the table
+            var CategoryFromDb = _db.Categories.Find(id);
+
+            if (CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(CategoryFromDb);
+        }
+
+        // POST: Category/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                // Custom error
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully!";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // GET: Category/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var CategoryFromDb = _db.Categories.Find(id);
+
+            if (CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(CategoryFromDb);
+        }
+
+        // POST: Category/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully!";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext // Don't forget to install 'EntityFrameworkCore'
+    {
+        /**
+         * When working with Entity Framework Core, there are two approaches:
+         * 
+         * 1. Code First Approach (this is what we're using) - we are writing the code of
+         * our model and based on this model, we will be creating the database.
+         * 
+         * 2. Database First Approach - database is already created and based on this database,
+         * you will be scaffolding models.
+         * 
+         * Package Manager Console Commands:
+         * 1. 'Add-Migration' = keeping a track of all database changes that are needed.
+         * Notes: 
+         * - To enable migrations, install 'Microsoft.EntityFrameworkCore.Tools' first.
+         * - Do not use any spaces when writing the name of the migration.
+         * 
+         * 2. 'Update-Database' = push the migrations to the database.
+         * 
+         */
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // For every model in your project, you need to create a DbSet
+        public DbSet<Category> Categories { get; set; }
+    }
+}
